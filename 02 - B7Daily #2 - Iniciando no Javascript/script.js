0d7d273117a7c4df4afc585642abb5e045c588a2
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-02-17 11:08:36 -0300

    operações básicas

--- 02 - B7Daily #2 - Iniciando no Javascript/script.js
+++ 02 - B7Daily #2 - Iniciando no Javascript/script.js
@@ -6,3 +6,15 @@ const idade = 90
 
 
 let total = numero1 + numero2
+
+
+let n1 = 10
+let n2 = 5;
+let soma = n1 + n2;
+console.log(soma)
+let subtrair = n1 - n2;
+console.log(subtrair)
+let multiplicar = n1 * n2;
+console.log(multiplicar)
+let dividir = n1 / n2;
+console.log(dividirma)
