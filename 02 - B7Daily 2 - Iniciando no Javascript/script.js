0a0bf50b7db51e8dd8a0d347db6cad55248fe070
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-02-17 11:30:23 -0300

    base html

---
+++ 02 - B7Daily 2 - Iniciando no Javascript/script.js
@@ -0,0 +1,39 @@
+let name = "renan"
+let numero1 = 10
+let numero2 = 30
+var teste = "nome"
+const idade = 90
+
+
+let total = numero1 + numero2
+
+
+let n1 = 10
+let n2 = 5;
+let soma = n1 + n2;
+console.log(soma)
+let subtrair = n1 - n2;
+console.log(subtrair)
+let multiplicar = n1 * n2;
+console.log(multiplicar)
+let dividir = n1 / n2;
+console.log(dividir)
+
+console.log("O total foi : "+ total);
+
+if (total > 50){
+    console.log("total foi maior que 50")
+}else{
+    console.log("total foi menor que 50")
+}
+
+//Array
+let ingredientes = [
+    'fdskla',
+    "fjdkslaç",
+    "jfdkslafjkas",
+    "dfsa",
+    "fjdkslafçd"
+]
+
+console.log(ingredientes)
